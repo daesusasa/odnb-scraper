6d9d3083ff7b663861579f46e184ced28808ab6a
daesusasa <david.susa@ggk.uni-giessen.de>
2020-02-15 21:32:34 +0100

    New version, 200

--- odnb-scraper.py
+++ odnb-scraper.py
@@ -3,49 +3,31 @@
 import sys
 import requests
 import argparse
-import html2text
-import re
+
 
 
 def main(arguments):
     res = []
 
-    parser = argparse.ArgumentParser(description='Extract author first paragraph from wikipedia.')
+    parser = argparse.ArgumentParser(description='Extract status result from ODNB.')
     # parser.add_argument('--exact', help="find an exact match of the game", action="store_true")
     # args = parser.parse_args()
 
     try:
 
-        with open("autores.txt") as fp, open('result.txt', 'a+') as wt:
+        with open("odnb-lista.txt") as fp, open('odnb-result.txt', 'a+') as wt:
             line = fp.readline()
             while line:
                 print(line)
 
-                # ?search=Keith+McCarthy&title=&go=Go&wprov=acrw1_0
-                # https://en.wikipedia.org/w/index.php?search=Keith+McCarthy&title=Special%3ASearch&go=Go&wprov=acrw1_0
-                # https://en.wikipedia.org/w/index.php?go=Go&search=Keith%2BMcCarthy&title=Special%253ASearch&ns0=1
-                payload = {'search': line.replace('\r\n', ''), 'title': 'Special:Search', 'go': 'Go',
-                           'wprov': 'acrw1_0'}
-                r = requests.get('https://en.wikipedia.org/w/index.php', params=payload)
+
+                # ODNB: search?q=Iris+Murdoch&searchBtn=Search&isQuickSearch=true
+                payload = {'q': line.replace('\r\n', ''), 'searchBtn': 'Search', 'isQuickSearch': 'true'}
+                r = requests.get('https://www.oxforddnb.com/search', params=payload)
                 r.raise_for_status()
-
-                h = html2text.HTML2Text()
-                h.ignore_links = True
-                h.single_line_break = True
-                h.ignore_tables = True
-                h.body_width = False
-                content = h.handle(r.content.decode('utf8'))
-
-                matches = re.findall("\*\*.*", content)
-                matches = list(filter(lambda x: "may not meet" not in x and "Learn how and when" not in x and "may refer to" not in x, matches))
-
-                if len(matches) == 0:
-                    wt.write(line.replace('\r\n', '') + ';' + '\n')
-                    line = fp.readline()
-                    continue
-
-                wt.write(line.replace('\r\n', '') + ';' + matches[0].encode('utf8') + '\n')
+                wt.write(line.replace('\n', '') + ';'+ str(r.status_code))
                 line = fp.readline()
+
     finally:
         fp.close()
         wt.close()
